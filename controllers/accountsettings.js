2735b9755bc4f23ffaa2de4c75f17c73f8da6fa3
Paddym1991 <paddym1991.pm@gmail.com>
2017-08-10 14:36:31 +0100

    account settings view added and file names changed

---
+++ controllers/accountsettings.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const accounts = require ('./accounts.js');
+const logger = require('../utils/logger');
+const userStore = require('../models/user-store');
+const uuid = require('uuid');
+
+const accountsettings = {
+  index(request, response) {
+    logger.info('account settings rendering');
+    const loggedInUser = accounts.getCurrentUser(request);
+    const viewData = {
+      title: 'Profile settings',
+      user: loggedInUser,
+    };
+    logger.info('abount to render', loggedInUser);
+    response.render('accountsettings', viewData);
+  },
+  update(request, response) {
+    let user = accounts.getCurrentUser(request);
+    const updateUser = request.body;
+
+    user.firstName = updateUser.firstName;
+    user.lastName = updateUser.lastName;
+    user.email = updateUser.email;
+    user.password = updateUser.password;
+  },
+};
+
+module.exports = accountsettings;
--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const accounts = require('./controllers/accounts.js');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const expense = require('./controllers/expense.js');
+const accountsettings = require('./controllers/accountsettings.js');
 
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
@@ -23,5 +24,8 @@ router.get('/about', about.index);
 router.get('/expense/:id', expense.index);
 router.get('/expense/:id/deletepayment/:paymentid', expense.deletePayment);
 router.post('/expense/:id/addpayment', expense.addPayment);
+
+router.get('/accountsettings', accountsettings.index);
+router.post('/accountsettings/update', accountsettings.update);
 
 module.exports = router;
